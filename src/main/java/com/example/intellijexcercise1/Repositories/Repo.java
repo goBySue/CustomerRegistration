5fc2932bdc13f1f8c3d80ba2b9804a521a109193
goBySue <jisu6.kim@gmail.com>
2023-12-13 01:56:24 -0800

    CSIS3275 Lab2

---
+++ src/main/java/com/example/intellijexcercise1/Repositories/Repo.java
@@ -0,0 +1,26 @@
+package com.example.intellijexcercise1.Repositories;
+
+import com.example.intellijexcercise1.Entities.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Repo {
+
+    List<Customer> list1 = new ArrayList<Customer>();
+
+    public ArrayList<Customer> initialdata()
+    {
+        Customer cc,dd;
+
+        cc = new Customer();
+        cc.setCname("Joe");
+        cc.setCemail("xxx@yahoo.com");
+
+        list1.add(cc);
+
+        return (ArrayList<Customer>) list1;
+    }
+}
